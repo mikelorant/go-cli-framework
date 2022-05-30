3e7485c8f682b00bc808035366962628a0201cc0
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2022-05-30 10:28:41 +1000

    DefValue was not matching value type

--- pkg/config/default.go
+++ pkg/config/default.go
@@ -3,11 +3,11 @@ package config
 func Defaults(bindings Bindings) Loader {
 	return LoaderFunc(func(cs ConfigSet) error {
 		for _, v := range bindings {
-			if v.flag.DefValue != "" {
+			if !v.flag.Changed {
 				if _, ok := cs[v.key]; !ok {
 					cs[v.key] = &ConfigValue{}
 				}
-				cs[v.key].value = v.flag.DefValue
+				cs[v.key].value = v.flag.Value
 				cs[v.key].origin = ConfigDefault
 			}
 		}
